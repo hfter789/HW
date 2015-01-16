1a052d5704dfc60e139227269b83e673798d93cd
hfter789 <hfter789@users.noreply.github.com>
2015-01-15 21:04:21 -0800

    done with part of quit func

--- project0/Server.js
+++ project0/Server.js
@@ -6,7 +6,9 @@ if(process.argv.length != 3){
 	console.log("Error. Usage: node server.js <portnum>");
 	return;
 }
-
+//the time that server would wait if not receiving any message from client
+var TIMEOUT = 30000
+//A magic number & version number in front of the message, discard message if not present
 var MAGIC = 0xC461
 var VERSION = 1
 var MIN_LENGTH = 12 //in bytes
@@ -19,6 +21,17 @@ var server = dgram.createSocket('udp4');
 //sessions is a map that maps from session id to # messages the session has received
 var sessions = {};
 
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', function() {
+  var cmd = process.stdin.read();
+  if (cmd && (cmd.toLowerCase() == "q" || cmd.toLowerCase() == "q\n")) {
+    process.stdout.write('ByeBye');
+    //TODO send all sessions a goodbye message
+    process.exit(1);
+  }
+});
+
 server.on('listening', function () {
     var address = server.address();
     console.log('Waiting on port ' + address.port);
@@ -29,6 +42,11 @@ server.on('message', function (message, remote) {
 });
 
 server.bind(PORT, HOST);
+
+function closeSession(id){
+	delete sessions[id];
+	console.log("timeout: " + id);
+}
 
 function processMsg(message,remote){
     console.log(remote.address + ':' + remote.port +' - ' + message);
@@ -49,4 +67,20 @@ function processMsg(message,remote){
 	console.log(sesID);
 
 	//2)examines session id: if exist, give to that session, else, create a new session
+	if(!(sesID in sessions)){
+		sesFields = {};
+		sesFields['Timer'] = setTimeout(closeSession, TIMEOUT, sesID);
+		sesFields['Address'] = remote;
+		sessions[sesID] = sesFields;
+	}else{
+		sesFields = sessions[sesID];
+		if(sesFields && sesFields["Timer"]){
+			//resetting the timer
+			clearTimeout(sesFields["Timer"]);
+			console.log("Timer Cleared");
+		}
+		sessions[sesID]['Timer'] = setTimeout(closeSession, TIMEOUT, sesID);
+	}
+
+	console.log(sessions);
 }
