4b04eb790f4f04dec3ba2bc0d8a6d7b1894247a6
hfter789 <hfter789@users.noreply.github.com>
2015-01-17 15:47:52 -0800

    Almost Done, need to fixed exit error

--- project0/ClientTest.js
+++ project0/ClientTest.js
@@ -17,6 +17,11 @@ var client = dgram.createSocket('udp4');
 var message = new Buffer('C461My KungFu is Good!(no good)');
 
 var client = dgram.createSocket('udp4');
+
+client.on('message', function (message, remote) {
+    console.log("UDP message received: " + message + ". From "+ remote.address);
+});
+
 client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
     if (err) throw err;
     console.log('UDP message sent to ' + HOST +':'+ PORT);
@@ -36,13 +41,46 @@ client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
     console.log('UDP message sent to ' + HOST +':'+ PORT);
 });
 
-message = new Buffer('C4611My KungFu is Good!');
-
-message = new Buffer('C4610103000001211B2D3C4A','hex');
+message = new Buffer('C4610111000001211B2D3C4A','hex');
 
 
 client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
     if (err) throw err;
     console.log('UDP message sent to ' + HOST +':'+ PORT);
-    client.close();
 });
+
+message = new Buffer('C4610100000001211B2D3C4A','hex');
+
+
+client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
+    if (err) throw err;
+    console.log('UDP message sent to ' + HOST +':'+ PORT);
+});
+
+message = new Buffer('C4610101000001211B2D3C4A','hex');
+var payload = new Buffer("I'M Very Smart");
+
+message = Buffer.concat([message, payload]);
+
+
+client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
+    if (err) throw err;
+    console.log('UDP message sent to ' + HOST +':'+ PORT);
+});
+
+message = new Buffer('C4610102000001211B2D3C4A','hex');
+
+
+client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
+    if (err) throw err;
+    console.log('UDP message sent to ' + HOST +':'+ PORT);
+});
+
+// //goodbye
+// message = new Buffer('C4610103000001211B2D3C4A','hex');
+
+
+// client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
+//     if (err) throw err;
+//     console.log('UDP message sent to ' + HOST +':'+ PORT);
+// });
--- project0/Server.js
+++ project0/Server.js
@@ -23,12 +23,18 @@ var sessions = {};
 
 process.stdin.setEncoding('utf8');
 
+//if stdin received input 'q', the program will send goodbye to all alive session
+//and exit.
 process.stdin.on('readable', function() {
   var cmd = process.stdin.read();
   if (cmd && (cmd.toLowerCase() == "q" || cmd.toLowerCase() == "q\n")) {
-    process.stdout.write('ByeBye');
-    //TODO send all sessions a goodbye message
-    process.exit(1);
+    process.stdout.write('ByeBye\n');
+    var count = 0;
+    for (var key in sessions){
+    	console.log(key);
+    	closeSession(key);
+    	count++;
+    }
   }
 });
 
@@ -43,9 +49,34 @@ server.on('message', function (message, remote) {
 
 server.bind(PORT, HOST);
 
+function sendMessage(remote, command, message){
+	var newMessage = new Buffer(message).slice(0,MIN_LENGTH);
+	newMessage[3] = command;
+	console.log("Length of new Message is: " + newMessage.length);
+	server.send(newMessage, 0, newMessage.length, remote.port, remote.address, function(err, bytes) {
+		if (err) throw err;
+		console.log('UDP message sent to ' + remote.address +':'+ remote.port);
+	});
+}
+
 function closeSession(id){
+	message = new Buffer('C461010300000000'+id,'hex');
+	var remote = sessions[id]['Address'];
+	clearTimeout(sessions[id]['Timer']);
+	console.log(remote);
+	sendMessage(remote, 3, message);
 	delete sessions[id];
-	console.log("timeout: " + id);
+	console.log("Session Closed " + id);
+}
+
+function resetTimer(id){
+	sesFields = sessions[id];
+	if(sesFields && sesFields["Timer"]){
+		//resetting the timer
+		clearTimeout(sesFields["Timer"]);
+		console.log("Timer Cleared");
+	}
+	sessions[id]['Timer'] = setTimeout(closeSession, TIMEOUT, id);
 }
 
 function processMsg(message,remote){
@@ -62,25 +93,50 @@ function processMsg(message,remote){
 	var cmd = msg[3];
 	var seqNum = parseInt(msg.slice(4,8).toString('hex',0,4));
 	var sesID = msg.slice(8,12).toString('hex',0,4);
-	console.log(cmd);
-	console.log(seqNum);
-	console.log(sesID);
-
+	/*
+	console.log("Remote is :");
+	console.log(remote);
+	console.log("Command is :" + cmd);
+	console.log("Sequence Number is: "+ seqNum);
+	console.log("Session ID is :" + sesID);
+	*/
 	//2)examines session id: if exist, give to that session, else, create a new session
 	if(!(sesID in sessions)){
+		if(cmd != 0){
+			console.log("Session not created.");
+			return;
+		}
 		sesFields = {};
 		sesFields['Timer'] = setTimeout(closeSession, TIMEOUT, sesID);
 		sesFields['Address'] = remote;
+		sesFields['SeqNum']
 		sessions[sesID] = sesFields;
 	}else{
-		sesFields = sessions[sesID];
-		if(sesFields && sesFields["Timer"]){
-			//resetting the timer
-			clearTimeout(sesFields["Timer"]);
-			console.log("Timer Cleared");
-		}
-		sessions[sesID]['Timer'] = setTimeout(closeSession, TIMEOUT, sesID);
+		resetTimer(sesID);
 	}
 
-	console.log(sessions);
+	// console.log("Session is :");
+	// console.log(sessions);
+
+	if(cmd == 0){
+		sendMessage(remote,0,message);
+	}else if(cmd == 1){
+		console.log("DATA");
+		//slice the message to get the payload.
+		//payload is everything from the end of header to the end of msg
+		var payload = msg.slice(MIN_LENGTH, msg.length);
+		process.stdout.write(payload +"\n");
+		sendMessage(remote,2,msg);
+	}else if(cmd == 2){
+		console.log("ALIVE");
+		resetTimer(sesID);
+	}else if(cmd == 3){
+		console.log("GOODBYE");
+		closeSession(sesID);
+	}else{
+		console.log("NOT RECOGNIZED");
+		return;
+	}
+
+
 }
